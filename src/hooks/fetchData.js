cac09a01bdf91650a5ce66a2b218d5d184e0ec6b
jun.guo <jun.guo@77hub.com>
2021-04-19 22:40:09 +0800

    feat: init

---
+++ src/hooks/fetchData.js
@@ -0,0 +1,12 @@
+import { ref } from "vue";
+
+/**
+ * 
+ * @param {*} url 
+ * @param {*} options 
+ * @param {*} responseResolver 
+ */
+export function useFetch(url, options, responseResolver, config={}) {
+  let data = ref([])
+  
+}
