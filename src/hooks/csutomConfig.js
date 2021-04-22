32193a9c1434548886b20eed435ae9b483d6f772
huanghui <hpysirius@gmail.com>
2021-04-22 22:45:12 +0800

    feat: 客流变化

--- src/hooks/csutomConfig.js
+++ src/hooks/csutomConfig.js
@@ -4,52 +4,62 @@ import { ref } from "vue";
  * 客流
  */
 export function useCustomConfig(rem) {
-  console.log(rem);
-
   const colors = ["#F05B49", "#16CAFF"];
   const customRef = ref({});
   customRef.value = {
     color: colors,
     tooltip: {
       trigger: "axis",
-      showContent: false
+      backgroundColor: "#090C12",
+      borderColor: "#60b4cd",
+      color: "#fff",
+      axisPointer: {
+        type: 'line',
+        lineStyle: {
+          type: 'solid',
+          color: '#637D83'
+        }
+      }
     },
     legend: {
       data: ["班次", "人次"],
       top: rem.ActualPixels(10),
-      left: rem.ActualPixels(10)
+      left: rem.ActualPixels(30),
+      textStyle: {
+        color: "#9ebfcb"
+      }
     },
     xAxis: {
       type: "category",
       nameLocation: "start",
+      boundaryGap: false,
       axisLine: {
-        show: false,
+        show: true,
         onZero: false,
         lineStyle: {
-          color: "#9EBFCB"
+          color: "#637D83"
         }
       },
       data: ["0", "3", "6", "9", "12", "15", "18", "21"]
     },
     yAxis: {
-      gridIndex: 0,
       axisLine: {
-        show: false,
+        show: true,
         onZero: false,
         lineStyle: {
-          color: "#9EBFCB"
+          color: "#637D83"
         }
       },
       splitLine: {
         show: true,
         lineStyle: {
-          color: ["#9EBFCB"]
+          color: ["#293E4D", '#293E4D', '#293E4D']
         }
       }
     },
     grid: {
       show: true,
-      borderColor: "#9EBFCB",
+      borderColor: "#293E4D",
       left: rem.ActualPixels(60),
       top: rem.ActualPixels(60),
       bottom: rem.ActualPixels(50),
